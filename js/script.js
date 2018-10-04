2e9ef22e1a399fc389f358718ee38bc214a4a201
Sam Costanzo <samcostanzo25@gmail.com>
2018-10-03 22:38:03 -0400

    10/3
    Pushing before trying a big change

--- js/script.js
+++ js/script.js
@@ -142,7 +142,6 @@ paymentSelect.addEventListener('change', () => {
 });
 
 
-
 // FORM VALIDATION************************************
 const RegisterButton = document.getElementById('registerButton');
 // IDEA-turn the border color of the name input red if left blank
@@ -151,20 +150,105 @@ const RegisterButton = document.getElementById('registerButton');
 
 // Function to check for the top nameField being left blank
 function validation() {
-const label = document.createElement('label');
-label.textContent = 'Please enter your name';
   if(nameField.value.length == 0) {
   nameField.style.borderColor = 'red';
-  nameField.appendChild(label);
-  alert('Please check the form again. Some sections can not be left blank');
-  console.log('It works!');
-  preventDefault()
+  nameField.placeholder = 'Please enter your name';
+  // Below was the code for adding a label in the HTML. Saved here just incase. Putting error message in placeholder instead.
+  // nameError.textContent = 'Please fill this out';
+  // nameError.style.color = 'red';
+  event.preventDefault();
+  nameField.focus();
 }
+// Same thing as above, but on the email field. Input turns red if left blank
+const emailInput = document.getElementById('mail');
+if(emailInput.value.length == 0) {
+  emailInput.style.borderColor = 'red';
+  emailInput.placeholder = 'Please enter your Email';
+}
+
+// REMINDER that we are still inside the validation function :)
+
+// If credit card option is chosen, user must provide a credit card number, zip code, and three digit CVV
+// Selecting the three inputs
+const creditCardNumberInput = document.getElementById('cc-num');
+const zipCodeInput = document.getElementById('zip');
+const CvvInput = document.getElementById('cvv');
+// Checking for credit card input
+if(creditCardNumberInput.value.length == 0){
+  creditCardNumberInput.style.borderColor = 'red';
+  creditCardNumberInput.placeholder = 'Please enter your credit card number';
+};
+// Checking for zip code input
+if(zipCodeInput.value.length == 0){
+  zipCodeInput.style.borderColor = 'red';
+  zipCodeInput.placeholder = 'Please fill out';
+};
+// Checking for CVV input
+if(CvvInput.value.length == 0){
+  CvvInput.style.borderColor = 'red';
+  CvvInput.placeholder = 'Please fill out';
+};
+
+// Check that the credit card number is between 13 and 16 digits
+// Store users submited credit card number in userCreditCardNumber
+const userCreditCardNumber = creditCardNumberInput.value;
+if(userCreditCardNumber.length <= 12) {
+  creditCardNumberInput.style.borderColor = 'red';
+  alert('Your credit card number must be between 13 and 16 digits');
+}
+if(userCreditCardNumber.length > 16) {
+  creditCardNumberInput.style.borderColor = 'red';
+  alert('Your credit card number must be between 13 and 16 digits');
+}
+
+// Check that the users zip code is exactly 5 digits 
+const userZipCode = zipCodeInput.value;
+if(userZipCode.length != 5) {
+  alert('Your zip code must be 5 digits long');
+  zipCodeInput.style.borderColor = 'red';
+}
+
+// Checks that the users credit card CVV is exactly 3 digits
+const userCvv = CvvInput.value;
+if(userCvv.length != 3) {
+  alert('Your credit card CVV must be 3 digits long');
+  CvvInput.style.borderColor = 'red';
+}
+
+
+
+// User must select atleast one checkbox in the register for activities section
+const allCheckBoxes = document.getElementsByName('choice');
+var hasChecked = false;
+for(var i = 0; i < allCheckBoxes.length; i++) {
+  if(allCheckBoxes[i].checked){
+    hasChecked = true;
+    break;
+  }
+}
+if(hasChecked == false){
+  alert('Please select at least one activity to attend');
+  return false;
+}
+  return true;
+}  // END OF VALIDATION FUNCTION
+
+
+
+
+// Function to check if email address formatted correctly. Inspired by Stackoverflow. Function is called in eventlistener below 
+function validateEmail(email) 
+{
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
 
 
 // Again, putting the function above into an event listener on the register button at the bottom.
 RegisterButton.addEventListener('click', () => {
   validation()
+  validateEmail()
 });
 
+// IDEA for getting credit card checks working only when credit card option is selected...
+// Move that code to a seperate function and call it in a if statement on an event listener on register button.
